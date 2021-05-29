5ef18c63688c751decd39be4868b0c7a3e1970d1
barikq <yar.barik@gmail.com>
2021-05-29 19:16:06 +0300

    'Двоичный' поиск

--- code/python/main.py
+++ code/python/main.py
@@ -5,7 +5,7 @@ import os
 import csv
 import time
 import cProfile
-from memory_profiler import profile
+# from memory_profiler import profile
 from PIL import Image
 from collections import deque
 
@@ -17,32 +17,57 @@ pxW = 752
 pxH = 752
 pxSize = pxW / width
 defPxDiameter = math.ceil(d * pxSize)
+data = []
+timeBruteOverall = 0
+timeDoubleSearch = 0
+timeRandomSpotOverall = 0
+timeRandomOverall = 0
+timeBreadthOverall = 0
+timeDoublePointOverall = 0
+timeDoubleBreadthOverall = 0
 
-def bruteForce(arrGS, arr):
+def profile(func):
+    """Decorator for run function profile"""
+    def wrapper(*args, **kwargs):
+        profile_filename = func.__name__ + '.prof'
+        profiler = cProfile.Profile()
+        result = profiler.runcall(func, *args, **kwargs)
+        profiler.dump_stats(profile_filename)
+        return result
+    return wrapper
+
+def bruteForce(arrGS, arr, folder_path, iii, generateImage):
   y = 0
   x = 0
   totalX = np.array([])
   totalY = np.array([])
+  dtotalX = []
+  dtotalY = []
   rowFlag = False
   rowFlag_2 = False
-  arrCopy = arr.copy()
+
+  if (generateImage):
+    arrCopy = arr.copy()
 
   for y, rows in enumerate(arrGS):
     colFlag = False
     for x, cols in enumerate(rows):
-
-      arrCopy[y][x] = [255, 255, 0]
+      if (generateImage):
+        arrCopy[y][x] = [255, 255, 0]
 
       if (cols[0] > 10 or cols[1] < 255):
 
-        arrCopy[y][x] = [255, 0, 0]
+        if (generateImage):
+          arrCopy[y][x] = [255, 0, 0]
 
-        totalX = np.append(totalX, x + 1)
-        totalY = np.append(totalY, y + 1)
+        # totalX = np.append(totalX, x + 1)
+        # totalY = np.append(totalY, y + 1)
+        dtotalX.append(x + 1)
+        dtotalY.append(y + 1)
         
         rowFlag_2 = True
         colFlag = True
-        if (len(totalX) == 1):
+        if (len(dtotalX) == 1):
           rowFlag = True
       else:
         if (colFlag):
@@ -52,21 +77,25 @@ def bruteForce(arrGS, arr):
       break
     
     rowFlag_2 = False
-  centerX = math.ceil(np.sum(totalX) / len(totalX))
-  centerY = math.ceil(np.sum(totalY) / len(totalY))
+  # centerX = math.ceil(np.sum(totalX) / len(totalX))
+  # centerY = math.ceil(np.sum(totalY) / len(totalY))
+  centerX = math.ceil(sum(dtotalX) / len(dtotalX))
+  centerY = math.ceil(sum(dtotalY) / len(dtotalY))
 
-  img = Image.fromarray(arrCopy)
-  img.save(os.path.join(folder_path, 'brute_image_' + str(iii) + '.png'))
+  if (generateImage):
+    img = Image.fromarray(arrCopy)
+    img.save(os.path.join(folder_path, 'brute_image_' + str(iii) + '.png'))
 
   return [centerX, centerY]
 
-def randomSpot(arrGS, arr):
+def randomSpot(arrGS, arr, folder_path, iii, generateImage):
   pointX = 0
   pointY = 0
   find = False
   rows = len(arrGS)
   cols = len(arrGS[0])
-  arrCopy = arr.copy()
+  if (generateImage):
+    arrCopy = arr.copy()
 
   columns = np.array(range(0, len(arrGS)))
   matrix = np.array([{'row': x, 'columns': columns} for x in range(len(columns))])
@@ -79,7 +108,9 @@ def randomSpot(arrGS, arr):
     randRow = matrix[randRowIndex]['row']
     randColumnIndex = random.randint(0, len(matrix[randRowIndex]['columns']) - 1)
     randColumn = matrix[randRowIndex]['columns'][randColumnIndex]
-    arrCopy[randRow][randColumn] = [255, 0, 0]
+
+    if (generateImage):
+      arrCopy[randRow][randColumn] = [255, 0, 0]
 
     if (arrGS[randRow][randColumn][0] > 10 or arrGS[randRow][randColumn][1] < 255):
       find = True
@@ -88,12 +119,13 @@ def randomSpot(arrGS, arr):
 
     matrix[randRowIndex]['columns'] = np.delete(matrix[randRowIndex]['columns'], randColumnIndex)
   
-  img = Image.fromarray(arrCopy)
-  img.save(os.path.join(folder_path, 'spot_image_' + str(iii) + '.png'))
+  if (generateImage):
+    img = Image.fromarray(arrCopy)
+    img.save(os.path.join(folder_path, 'spot_image_' + str(iii) + '.png'))
 
   return [pointX, pointY]
 
-def randPick(arrGS, point, arr):
+def randPick(arrGS, point, arr, folder_path, iii, generateImage):
   leftTopX = point[0] - defPxDiameter
   leftTopY = point[1] - defPxDiameter
   leftBottomX = point[0] - defPxDiameter;
@@ -123,25 +155,32 @@ def randPick(arrGS, point, arr):
   x = 0
   totalX = np.array([])
   totalY = np.array([])
+  dtotalX = []
+  dtotalY = []
   rowFlag = False
   rowFlag_2 = False
-  arrCopy = arr.copy()
+  if (generateImage):
+    arrCopy = arr.copy()
 
   for y in range(quadroY[0], quadroY[2]):
     colFlag = False
     for x in range(quadroX[0], quadroX[2]):
-      arrCopy[y][x] = [255, 255, 0]
+      if (generateImage):
+        arrCopy[y][x] = [255, 255, 0]
 
       if (arrGS[y][x][0] > 10 or arrGS[y][x][1] < 255):
-        arrCopy[y][x] = [255, 0, 0]
+        if (generateImage):
+          arrCopy[y][x] = [255, 0, 0]
 
-        totalX = np.append(totalX, x + 1)
-        totalY = np.append(totalY, y + 1)
+        # totalX = np.append(totalX, x + 1)
+        # totalY = np.append(totalY, y + 1)
+        dtotalX.append(x + 1)
+        dtotalY.append(y + 1)
 
         colFlag = True
         rowFlag_2 = True
 
-        if (len(totalX) == 1):
+        if (len(dtotalX) == 1):
           rowFlag = True
       else:
         if (colFlag):
@@ -152,25 +191,31 @@ def randPick(arrGS, point, arr):
     
     rowFlag_2 = False
 
-  centerX = math.ceil(np.sum(totalX) / len(totalX))
-  centerY = math.ceil(np.sum(totalY) / len(totalY))
+  # centerX = math.ceil(np.sum(totalX) / len(totalX))
+  # centerY = math.ceil(np.sum(totalY) / len(totalY))
+  centerX = math.ceil(sum(dtotalX) / len(dtotalX))
+  centerY = math.ceil(sum(dtotalY) / len(dtotalY))
 
-  img = Image.fromarray(arrCopy)
-  img.save(os.path.join(folder_path, 'random_image_' + str(iii) + '.png'))
+  if (generateImage):
+    img = Image.fromarray(arrCopy)
+    img.save(os.path.join(folder_path, 'random_image_' + str(iii) + '.png'))
 
   return [centerX, centerY]
 
-def breadthSearch(arrGS, point, arr):
-  arrCopy = arr.copy()
+def breadthSearch(arrGS, point, arr, folder_path, iii, generateImage):
+  if (generateImage):
+    arrCopy = arr.copy()
+
   totalX = np.array([])
   totalY = np.array([])
+  dtotalX = []
+  dtotalY = []
   point = tuple(point)
   frontier = deque()
   frontier.append(point)
   reached = set()
   reached.add(point)
 
-  print(len(frontier))
   while not len(frontier) == 0:
     current = frontier.popleft()
     neighboors = [
@@ -195,77 +240,179 @@ def breadthSearch(arrGS, point, arr):
         if (not(arrGS[next[1]][next[0]][0] > 10 or arrGS[next[1]][next[0]][1] < 255)):
           continue
         
-        arrCopy[next[1], next[0]] = [255, 0, 0]
+        if (generateImage):
+          arrCopy[next[1], next[0]] = [255, 0, 0]
+
         frontier.append(next)
         reached.add(next)
-        totalX = np.append(totalX, next[0] + 1)
-        totalY = np.append(totalY, next[1] + 1)
+        # totalX = np.append(totalX, next[0] + 1)
+        # totalY = np.append(totalY, next[1] + 1)
+        dtotalX.append(next[0] + 1)
+        dtotalY.append(next[1] + 1)
 
-  centerX = math.ceil(np.sum(totalX) / len(totalX))
-  centerY = math.ceil(np.sum(totalY) / len(totalY))
+  # centerX = math.ceil(np.sum(totalX) / len(totalX))
+  # centerY = math.ceil(np.sum(totalY) / len(totalY))
+  centerX = math.ceil(sum(dtotalX) / len(dtotalX))
+  centerY = math.ceil(sum(dtotalY) / len(dtotalY))
 
-  img = Image.fromarray(arrCopy)
-  img.save(os.path.join(folder_path, 'breadth_image_' + str(iii) + '.png'))
+  if (generateImage):
+    img = Image.fromarray(arrCopy)
+    img.save(os.path.join(folder_path, 'breadth_image_' + str(iii) + '.png'))
 
   return [centerX, centerY]
 
-data = []
-timeBruteOverall = 0
-timeRandomSpotOverall = 0
-timeRandomOverall = 0
-timeBreadthOverall = 0
+def doubleSearch(arrGS, arr, folder_path, iii, generateImage):
+  y = 0
+  x = 0
+  pointX = 0
+  pointY = 0
+  flag = False
 
-for iii in range(1, 501):
-  rel_path = ('../images/circle_' + str(iii) + '.png')
-  abs_path = os.path.join(script_dir, rel_path)
-  folder_rel_path = ('./images/image_' + str(iii))
-  folder_path = os.path.join(script_dir, folder_rel_path)
-  os.makedirs(os.path.join(script_dir, folder_path), exist_ok=True)
+  if (generateImage):
+    arrCopy = arr.copy()
 
-  arr = np.asarray(Image.open(abs_path))
-  arrGS = np.asarray(Image.open(abs_path).convert('LA'))
+  for y in range(math.floor(len(arrGS) / 2)):
+    y = y * 2
+    for x in range(math.floor(len(arrGS[y]) / 2)):
+      x = x * 2
 
-  start_1 = time.time()
-  bruteCenter = bruteForce(arrGS, arr)
-  end_1 = time.time()
-  timeBruteOverall += end_1 - start_1
+      if (generateImage):
+        arrCopy[y][x] = [255, 255, 0]
 
-  start_2 = time.time()
-  randPoint = randomSpot(arrGS, arr)
-  end_2 = time.time()
-  timeRandomSpotOverall += end_2 - start_2
+      if (arrGS[y][x][0] > 10 or arrGS[y][x][1] < 255):
 
-  start_3 = time.time()
-  randCenter = randPick(arrGS, randPoint, arr)
-  end_3 = time.time()
-  timeRandomOverall += end_3 - start_3
+        if (generateImage):
+          arrCopy[y][x] = [255, 0, 0]
 
-  start_4 = time.time()
-  breadthCenter = breadthSearch(arrGS, randPoint, arr)
-  end_4 = time.time()
-  timeBreadthOverall += end_4 - start_4
+        pointX = x;
+        pointY = y;
+        flag = True
+        break
+    
+    if (flag):
+      break
+        
+  if (generateImage):
+    img = Image.fromarray(arrCopy)
+    img.save(os.path.join(folder_path, 'double_image_' + str(iii) + '.png'))
 
-  print(iii, " |  ", bruteCenter, "  |  " , randCenter, "  |  " , breadthCenter)
-  data.append((
-    str(bruteCenter[0]),
-    str(bruteCenter[1]),
-    "%.4f" %(end_1 - start_1) + ' s',
-    str(randCenter[0]),
-    str(randCenter[1]),
-    "%.4f" %(end_2 - start_2) + ' s',
-    str(breadthCenter[0]),
-    str(breadthCenter[1]),
-    "%.4f" %(end_3 - start_3) + ' s',
-    "%.4f" %(end_4 - start_4) + ' s',
-  ))
+  return [pointX, pointY]
+
+@profile
+def imageProcessing():
+  for iii in range(1, 15):
+    rel_path = ('../images/circle_' + str(iii) + '.png')
+    abs_path = os.path.join(script_dir, rel_path)
+    folder_rel_path = ('./images/image_' + str(iii))
+    folder_path = os.path.join(script_dir, folder_rel_path)
+    os.makedirs(os.path.join(script_dir, folder_path), exist_ok=True)
+
+    arr = np.asarray(Image.open(abs_path))
+    arrGS = np.asarray(Image.open(abs_path).convert('LA'))
+
+    generateImage = False
+
+# =============================================================
+
+    start_1 = time.time()
+    bruteCenter = bruteForce(arrGS, arr, folder_path, iii, generateImage)
+    end_1 = time.time()
+
+    global timeBruteOverall
+    timeBruteOverall += end_1 - start_1
+    timeBruteString = "%.4f" %(end_1 - start_1) + ' s',
+
+# =============================================================
+
+    start_2 = time.time()
+    randPoint = randomSpot(arrGS, arr, folder_path, iii, generateImage)
+    end_2 = time.time()
+
+    global timeRandomSpotOverall
+    timeRandomSpotOverall += end_2 - start_2
+    timeSpotString = "%.4f" %(end_2 - start_2) + ' s',
+
+# =============================================================
+
+    start_3 = time.time()
+    randCenter = randPick(arrGS, randPoint, arr, folder_path, iii, generateImage)
+    end_3 = time.time()
+
+    global timeRandomOverall
+    timeRandomOverall += end_3 - start_3
+    timeRandString = "%.4f" %(end_3 - start_3) + ' s',
+
+# =============================================================
+
+    start_4 = time.time()
+    breadthCenter = breadthSearch(arrGS, randPoint, arr, folder_path, iii, generateImage)
+    end_4 = time.time()
+
+    global timeBreadthOverall
+    timeBreadthOverall += end_4 - start_4
+    timeSBreadthtring = "%.4f" %(end_4 - start_4) + ' s',
+
+# =============================================================
+
+    start_5 = time.time()
+    doublePoint = doubleSearch(arrGS, arr, folder_path, iii, generateImage)
+    end_5 = time.time()
+
+    global timeDoublePointOverall
+    timeDoublePointOverall += end_5 - start_5
+    timeDoublePointString = "%.4f" %(end_5 - start_5) + ' s',
+
+# =============================================================
+
+    start_6 = time.time()
+    doubleBreadthCenter = breadthSearch(arrGS, doublePoint, arr, folder_path, iii, generateImage)
+    end_6 = time.time()
+
+    global timeDoubleBreadthOverall
+    timeDoubleBreadthOverall += end_6 - start_6
+    timeDoubleBreadthString = "%.4f" %(end_6 - start_6) + ' s',
+
+# =============================================================
+
+    print(iii,
+    "  |  brute ", bruteCenter, timeBruteString,
+    "  |  double brdth ", doubleBreadthCenter, timeDoubleBreadthString, 
+    "  |  rand " , randCenter, timeRandString, 
+    "  |  breadth " , breadthCenter, timeSBreadthtring, 
+    "  |  rand spot ", timeSpotString, 
+    "  |  double spot ", timeDoublePointString)
+    
+    data.append((
+      str(bruteCenter[0]),
+      str(bruteCenter[1]),
+      ''.join(timeBruteString),
+      str(randCenter[0]),
+      str(randCenter[1]),
+      ''.join(timeRandString),
+      str(breadthCenter[0]),
+      str(breadthCenter[1]),
+      ''.join(timeSBreadthtring),
+      str(doubleBreadthCenter[0]),
+      str(doubleBreadthCenter[1]),
+      ''.join(timeDoubleBreadthString),
+      ''.join(timeSpotString),
+      ''.join(timeDoublePointString),
+    ))
+
+imageProcessing()
 
 timeBruteOverall = ("%.4f" % (timeBruteOverall)) + ' s'
 timeRandomOverall = ("%.4f" % (timeRandomOverall + timeRandomSpotOverall)) + ' s'
 timeBreadthOverall = ("%.4f" % (timeBreadthOverall + timeRandomSpotOverall)) + ' s'
 timeRandomSpotOverall = ("%.4f" % (timeRandomSpotOverall)) + ' s'
+timeDoubleBreadthOverall = ("%.4f" % (timeDoubleBreadthOverall + timeDoublePointOverall)) + ' s'
+timeDoublePointOverall = ("%.4f" % (timeDoublePointOverall)) + ' s'
 
-data.insert(0, ('bruteX', 'bruteY', 'time', 'randX', 'randY', 'time', 'breadthX', 'breadthY', 'time', 'spot time')) 
-data.insert(0, ('', '', timeBruteOverall, '', '', timeRandomOverall, '', '', timeBreadthOverall, timeRandomSpotOverall))
+times = ["Brute:", timeBruteOverall, "Random:", timeRandomOverall, "Breadth:", timeBreadthOverall, "Double breadth:", timeDoubleBreadthOverall, "Random Spot:", timeRandomSpotOverall, "Double spot:", timeDoublePointOverall]
+print(times)
+
+data.insert(0, ('bruteX', 'bruteY', 'time', 'randX', 'randY', 'time', 'breadthX', 'breadthY', 'time', 'dBrdthX', 'dBrdthY', 'time', 'spot time', 'double time')) 
+data.insert(0, ('', '', timeBruteOverall, '', '', timeRandomOverall, '', '', timeBreadthOverall, '', '', timeDoubleBreadthOverall, timeRandomSpotOverall, timeDoublePointOverall))
 
 with open('compare_py.csv', "w", newline="") as file:
   writer = csv.writer(file)
